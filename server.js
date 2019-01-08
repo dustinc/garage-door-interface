430a46cfb6b86fd3c03b6586fca52ce539a0779c
Dustin Coffey <dustin@cass-solutions.com>
2019-01-08 14:03:19 -0500

    init

---
+++ server.js
@@ -0,0 +1,46 @@
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+
+
+// Redis
+
+const redis = require('redis');
+const client = redis.createClient(process.env.REDIS_URI);
+
+
+// Routes
+
+app.get('/', function(req, res) {
+  res.send('Stormcloud Garage Door');
+});
+
+
+
+
+// Catch-all Route Error Handler
+
+app.use(function(err, req, res, next) {
+  console.error(err);
+  let message = err.message;
+  if (err.errors) {
+    message += ' ' + _.pluck(err.errors, 'message').join(' ');
+  }
+  res.status(res.statusCode==200?(err.status?err.status:500):res.statusCode);
+  return res.json(_.defaults({ message: message }, err, {
+    message: message,
+    type: err.type,
+    name: err.name,
+    code: err.code,
+    status: res.statusCode
+  }));
+});
+
+
+
+
+server.listen(process.env.PORT, async function() {
+  console.log('Stormcloud Garage Door listening on', process.env.PORT, process.env.NODE_ENV);
+});
